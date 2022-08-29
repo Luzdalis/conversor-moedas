27928f93ca2bb487bdaa1dc6c729179742eb4cb1
Luzdalis Lopez <lopezluzdalis@gmail.com>
2022-08-29 12:49:32 -0300

    Alguna alteracao na classe Main

--- src/br/com/alura/conversor/Main.java
+++ src/br/com/alura/conversor/Main.java
@@ -8,5 +8,11 @@ public class Main {
 		Conversor conv = new Conversor("CONVERSOR_REAIS_LIBRAS_ESTERLINAS", 5);
 		double r = conv.convertirMoeda();	
 		System.out.printf("Resultado:  %.2f", r );
+		
+		System.out.println();
+		
+		Conversor conv1 = new Conversor("CONVERSOR_REAIS_PESO_CHILENO", 5);
+		double r1 = conv1.convertirMoeda();	
+		System.out.printf("Resultado:  %.2f", r1 );
 	}
 }
